9d739ad2c72e77dea3e33777799336b132374b57
Murtuza <murtuza.shergadwala@workday.com>
2024-01-25 13:43:07 -0800

    made updates

--- risk_eval.py
+++ risk_eval.py
@@ -13,20 +13,49 @@ credentials = service_account.Credentials.from_service_account_info(
 client = gspread.authorize(credentials)
 
 # Define your risk categories
-risk_categories = ["Low Risk", "Medium Risk", "Moderately-High Risk", "High Risk"]
+
+r1 = "Low Risk"
+r2 = "Medium Risk"
+r3 = "Moderately-High Risk"
+r4 = "High Risk"
+
+risk_categories = [r1, r2, r3, r4]
+
+sit1 = """Automated Job Candidate Selection: An AI system that makes final hiring decisions by evaluating candidates'
+     resumes and interview performances."""
+
+sit2 = """Automated Insurance Claim Adjudication: AI systems that evaluate and make final decisions on insurance 
+    claims."""
+
+sit3 = """Skills Development Conversation Preparation for Managers using Generative AI."""
+
+sit4 = """Gigs Suggestions to Workers on Career Hub."""
+
+sit5 = """GenAI tools for generating job descriptions."""
+
+sit6 = """Skill suggestions to workers."""
+
+sit7 = """Expenditure trend analysis using AI"""
+
+sit8 = """Monitoring financial transactions for fraud and anomalies"""
+
+sit9 = """Employee Engagement Analysis: AI can analyze employee feedback from surveys"""
 
 # List of situations
-situations = [
-    "Dummy Situation 1: AI is just suggesting ideas.",
-    "Dummy Situation 2: AI is making decisions for you.",
-    # Add more situations as needed
-]
+situations = [globals()[f"sit{i}"] for i in range(1, 10)]
 
 # Define the correct answers
 correct_answers = {
-    "Situation 1": "Low Risk",
-    "Situation 2": "High Risk",
-    # Add correct answers for more situations as needed
+    sit1: r4,
+    sit2: r4,
+    sit3: r3,
+    sit4: r3,
+    sit5: r2,
+    sit6: r2,
+    sit7: r1,
+    sit8: r1,
+    sit9: r1
+  # Add correct answers for more situations as needed
 }
 
 COL_RANGE = 'A:D'
@@ -55,7 +84,7 @@ def submit():
     rows = len(q_list)
     # print(st.session_state.query)
     if q_list[rows - 1] == st.session_state['user_name']:
-        sheet.update(f'D{rows}', st.session_state.feedback)
+        sheet.update(range_name=f'D{rows}', values=[[st.session_state.feedback]])
     return
 
 
@@ -78,9 +107,9 @@ def store_query():
 def display_situations(situations):
     user_choices = {}
     for i, situation in enumerate(situations):
-        st.text(situation)
+        st.write(f"Situation {i+1}: " + situation)
         choice = st.selectbox(f"Choose category for Situation {i + 1}", risk_categories, key=i, disabled=st.session_state.displayon)
-        user_choices[f"Situation {i + 1}"] = choice
+        user_choices[globals()[f"sit{i+1}"]] = choice
     return user_choices
 
 # Function to compare user choices with correct answers
@@ -104,21 +133,23 @@ def main():
     if st.session_state['user_name']:
         st.write(f"Welcome, {st.session_state['user_name']}!")
 
-    st.session_state["choices"] = display_situations(situations)
-    st.button("Reveal", key="reveal", on_click=store_query)
+        st.write("In the following, you will see a bunch of situations and a drop down menu for you to choose the risk category of the situation. Make the appropriate choices and then reveal the answers and see how many you got right!")
 
-    if st.session_state.displayon:
-        results = compare_choices(st.session_state["choices"], correct_answers)
-        for situation, (user_choice, correct_choice) in results.items():
-            if user_choice == correct_choice:
-                st.success(f"{situation}: Correct! You chose {user_choice}")
-            else:
-                st.error(
-                    f"{situation}: Incorrect. You chose {user_choice}, but the correct category is {correct_choice}")
-    st.subheader("Your Thoughts")
-    st.text_input("Please provide feedback!", key="widget", on_change=submit)
-    if st.session_state.feedback:
-        st.write(f"Thank you for your feedback: {st.session_state.feedback}")
+        st.session_state["choices"] = display_situations(situations)
+        st.button("Reveal", key="reveal", on_click=store_query)
+
+        if st.session_state.displayon:
+            results = compare_choices(st.session_state["choices"], correct_answers)
+            for i, (situation, (user_choice, correct_choice)) in enumerate(results.items()):
+                if user_choice == correct_choice:
+                    st.success(f"For Situation {i+1}: Correct! You chose {user_choice}")
+                else:
+                    st.error(
+                        f"For Situation {i+1}: Incorrect. You chose {user_choice}, but the correct category is {correct_choice}")
+            st.subheader("Your Thoughts")
+            st.text_input("Please provide feedback!", key="widget", on_change=submit)
+            if st.session_state.feedback:
+                st.write(f"Thank you for your feedback: {st.session_state.feedback}")
 
 
 # Run the app
